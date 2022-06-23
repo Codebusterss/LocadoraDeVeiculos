8852054bfd491cbe520fb9ebd0a2f9b34d3a8681
Luan Vale <luancabral5566@gmail.com>
2022-06-23 14:51:16 -0300

    feat: adicionado tabelas do banco de dados e o projeto.

---
+++ LocadoraDeVeiculos.Projeto.BancoDeDados/dbo/Tables/CLIENTE.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[CLIENTE] (
+    [ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [NOME]        VARCHAR (300) NOT NULL,
+    [CPF]         VARCHAR (300) NULL,
+    [CNPJ]        VARCHAR (300) NULL,
+    [ENDERECO]    VARCHAR (300) NOT NULL,
+    [CNH]         VARCHAR (300) NULL,
+    [TELEFONE]    VARCHAR (300) NOT NULL,
+    [TIPOCLIENTE] BIT           NOT NULL,
+    CONSTRAINT [PK_CLIENTE] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
