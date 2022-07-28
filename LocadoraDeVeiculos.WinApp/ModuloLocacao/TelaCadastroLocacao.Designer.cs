95e7ab407013c27fdc4c67a2bfc72b49d1e43d5b
Mariaeduardaaz <Mariaeduardaaz1307@gmail.com>
2022-07-28 15:56:43 -0300

    feat: Adicionei a tela de Locação

--- LocadoraDeVeiculos.WinApp/ModuloLocacao/TelaCadastroLocacao.Designer.cs
+++ LocadoraDeVeiculos.WinApp/ModuloLocacao/TelaCadastroLocacao.Designer.cs
@@ -28,12 +28,301 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxFuncionario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbBoxCliente = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbBoxCondutor = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbBoxVeiculo = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimeDataLocacao = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbBoxPlanoCobranca = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtBoxKMveiculo = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dateTimeDataDevolucao = new System.Windows.Forms.DateTimePicker();
+            this.label10 = new System.Windows.Forms.Label();
+            this.checkedListBoxTaxas = new System.Windows.Forms.CheckedListBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.checkBoxTotal = new System.Windows.Forms.CheckBox();
+            this.checkBoxTerceiros = new System.Windows.Forms.CheckBox();
+            this.checkBoxIndividual = new System.Windows.Forms.CheckBox();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Funcionário:";
+            // 
+            // txtBoxFuncionario
+            // 
+            this.txtBoxFuncionario.Location = new System.Drawing.Point(200, 36);
+            this.txtBoxFuncionario.Name = "txtBoxFuncionario";
+            this.txtBoxFuncionario.Size = new System.Drawing.Size(196, 31);
+            this.txtBoxFuncionario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cliente:";
+            // 
+            // cbBoxCliente
+            // 
+            this.cbBoxCliente.FormattingEnabled = true;
+            this.cbBoxCliente.Location = new System.Drawing.Point(200, 99);
+            this.cbBoxCliente.Name = "cbBoxCliente";
+            this.cbBoxCliente.Size = new System.Drawing.Size(196, 33);
+            this.cbBoxCliente.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(578, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Condutor:";
+            // 
+            // cbBoxCondutor
+            // 
+            this.cbBoxCondutor.FormattingEnabled = true;
+            this.cbBoxCondutor.Location = new System.Drawing.Point(676, 41);
+            this.cbBoxCondutor.Name = "cbBoxCondutor";
+            this.cbBoxCondutor.Size = new System.Drawing.Size(196, 33);
+            this.cbBoxCondutor.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(125, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Veículo:";
+            // 
+            // cbBoxVeiculo
+            // 
+            this.cbBoxVeiculo.FormattingEnabled = true;
+            this.cbBoxVeiculo.Location = new System.Drawing.Point(200, 165);
+            this.cbBoxVeiculo.Name = "cbBoxVeiculo";
+            this.cbBoxVeiculo.Size = new System.Drawing.Size(196, 33);
+            this.cbBoxVeiculo.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Data de Locação:";
+            // 
+            // dateTimeDataLocacao
+            // 
+            this.dateTimeDataLocacao.Location = new System.Drawing.Point(200, 231);
+            this.dateTimeDataLocacao.Name = "dateTimeDataLocacao";
+            this.dateTimeDataLocacao.Size = new System.Drawing.Size(264, 31);
+            this.dateTimeDataLocacao.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(505, 102);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(165, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Plano de Cobrança:";
+            // 
+            // cbBoxPlanoCobranca
+            // 
+            this.cbBoxPlanoCobranca.FormattingEnabled = true;
+            this.cbBoxPlanoCobranca.Location = new System.Drawing.Point(676, 102);
+            this.cbBoxPlanoCobranca.Name = "cbBoxPlanoCobranca";
+            this.cbBoxPlanoCobranca.Size = new System.Drawing.Size(196, 33);
+            this.cbBoxPlanoCobranca.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(540, 168);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 25);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "KM do Veículo:";
+            // 
+            // txtBoxKMveiculo
+            // 
+            this.txtBoxKMveiculo.Location = new System.Drawing.Point(676, 167);
+            this.txtBoxKMveiculo.Name = "txtBoxKMveiculo";
+            this.txtBoxKMveiculo.Size = new System.Drawing.Size(196, 31);
+            this.txtBoxKMveiculo.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(503, 231);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(167, 25);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Data de Devolução:";
+            // 
+            // dateTimeDataDevolucao
+            // 
+            this.dateTimeDataDevolucao.Location = new System.Drawing.Point(676, 232);
+            this.dateTimeDataDevolucao.Name = "dateTimeDataDevolucao";
+            this.dateTimeDataDevolucao.Size = new System.Drawing.Size(264, 31);
+            this.dateTimeDataDevolucao.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Sylfaen", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(125, 289);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(66, 26);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Taxas:";
+            // 
+            // checkedListBoxTaxas
+            // 
+            this.checkedListBoxTaxas.FormattingEnabled = true;
+            this.checkedListBoxTaxas.Location = new System.Drawing.Point(200, 288);
+            this.checkedListBoxTaxas.Name = "checkedListBoxTaxas";
+            this.checkedListBoxTaxas.Size = new System.Drawing.Size(326, 172);
+            this.checkedListBoxTaxas.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Sylfaen", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(578, 288);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(76, 26);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Seguro:";
+            // 
+            // checkBoxTotal
+            // 
+            this.checkBoxTotal.AutoSize = true;
+            this.checkBoxTotal.Location = new System.Drawing.Point(660, 288);
+            this.checkBoxTotal.Name = "checkBoxTotal";
+            this.checkBoxTotal.Size = new System.Drawing.Size(75, 29);
+            this.checkBoxTotal.TabIndex = 26;
+            this.checkBoxTotal.Text = "Total";
+            this.checkBoxTotal.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTerceiros
+            // 
+            this.checkBoxTerceiros.AutoSize = true;
+            this.checkBoxTerceiros.Location = new System.Drawing.Point(660, 323);
+            this.checkBoxTerceiros.Name = "checkBoxTerceiros";
+            this.checkBoxTerceiros.Size = new System.Drawing.Size(106, 29);
+            this.checkBoxTerceiros.TabIndex = 27;
+            this.checkBoxTerceiros.Text = "Terceiros";
+            this.checkBoxTerceiros.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIndividual
+            // 
+            this.checkBoxIndividual.AutoSize = true;
+            this.checkBoxIndividual.Location = new System.Drawing.Point(660, 358);
+            this.checkBoxIndividual.Name = "checkBoxIndividual";
+            this.checkBoxIndividual.Size = new System.Drawing.Size(115, 29);
+            this.checkBoxIndividual.TabIndex = 28;
+            this.checkBoxIndividual.Text = "Individual";
+            this.checkBoxIndividual.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(702, 462);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(120, 48);
+            this.btnGravar.TabIndex = 29;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(828, 462);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(120, 48);
+            this.btnCancelar.TabIndex = 30;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // TelaCadastroLocacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(960, 513);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.checkBoxIndividual);
+            this.Controls.Add(this.checkBoxTerceiros);
+            this.Controls.Add(this.checkBoxTotal);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.checkedListBoxTaxas);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dateTimeDataDevolucao);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtBoxKMveiculo);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbBoxPlanoCobranca);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimeDataLocacao);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbBoxVeiculo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbBoxCondutor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbBoxCliente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxFuncionario);
+            this.Controls.Add(this.label1);
+            this.Name = "TelaCadastroLocacao";
             this.Text = "TelaCadastroLocacao";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtBoxFuncionario;
+        private Label label2;
+        private ComboBox cbBoxCliente;
+        private Label label4;
+        private ComboBox cbBoxCondutor;
+        private Label label5;
+        private ComboBox cbBoxVeiculo;
+        private Label label6;
+        private DateTimePicker dateTimeDataLocacao;
+        private Label label7;
+        private ComboBox cbBoxPlanoCobranca;
+        private Label label8;
+        private TextBox txtBoxKMveiculo;
+        private Label label9;
+        private DateTimePicker dateTimeDataDevolucao;
+        private Label label10;
+        private CheckedListBox checkedListBoxTaxas;
+        private Label label11;
+        private CheckBox checkBoxTotal;
+        private CheckBox checkBoxTerceiros;
+        private CheckBox checkBoxIndividual;
+        private Button btnGravar;
+        private Button btnCancelar;
     }
 }
